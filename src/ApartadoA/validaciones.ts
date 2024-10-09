8be5b1ea744a658db5311c149ecef2701fc062cf
Camila Villalba <cami_vp_23@hotmail.com>
2024-10-09 19:53:26 +0200

    creating funciton pintar and editing validarIBAN

--- src/ApartadoA/validaciones.ts
+++ src/ApartadoA/validaciones.ts
@@ -1,5 +1,6 @@
 /*
 WW00 0000 0000 00 0000000000
+ES91 2100 0418 4502 0005 1332
 
 WW: código de país 
 00: dígito de control 
@@ -13,19 +14,37 @@ WW: código de país
 import {electronicFormatIBAN, isValidIBAN,} from "ibantools"
 
 export const validarFormatoIBAN = (value : string) : boolean => {
-    const patron =/^[A-Za-z]{2}\d{2}(\s|-)?(\d{4}(\s|-)?){2}\d{2}(\s|-)?\d{10}$/;
+    const patron =/^[A-Za-z]{2}\d{2}(\s|-)?(\d{4}(\s|-)?){2}(\d{2}(\s|-)?\d{10}|(\d{4}(\s|-)?){3})$/;
     return patron.test(value)
 }
 
-
-const validarIBAN = (value : string) : boolean => {
+const pintar = (text :string) => {
+    const contenedor = document.getElementById("container-elements")
+    const parrafo = document.createElement("p");
+    parrafo.textContent = text;
+    contenedor?.appendChild(parrafo)
+    
+}
+const validarIBAN = (value : string) => {
 
     const esFormatoValido =validarFormatoIBAN(value);
 
-    if(!esFormatoValido){
+    if(esFormatoValido){
+    pintar("El formato del IBAN es correcto")
+    //AQUI DEBERIA IR LA COMPROBACION DE SI ES VALIDO, CUANDO QUITEMOS EL RETURN LO METO AQUI
 
+    } else {
+    pintar("El formato del IBAN no es correcto")
+    
     }
     const iban = electronicFormatIBAN(value.replace(/\s+/g,"")) as string;
+    const esValido = isValidIBAN(iban);
+
+    if(esValido){
+        pintar("El IBAN es válido")
+    } else {
+        pintar("El IBAN no es válido")
+    }
     
     return isValidIBAN(iban)
 }
