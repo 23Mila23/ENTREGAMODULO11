ea02a861c44d58b66b172c9e533d3b1965c30b1a
Camila Villalba <cami_vp_23@hotmail.com>
2024-10-10 14:51:49 +0200

    add functions pintarURLS and extraerImagenes

--- src/ApartadoB/main.ts
+++ src/ApartadoB/main.ts
@@ -138,18 +138,53 @@ const htmltest = `
   </body>
 `;
 
-const buscarTagsImg = (): string[] => {
+const buscarTagsImg = (html: string): RegExpMatchArray => {
   const patronImg = /<img\s+(?<url>src=["']([^"']+)["'])[^>]*>/gim;
-  const coincidencias = htmltest.match(patronImg);
+  const coincidencias = html.match(patronImg);
+  if (!coincidencias) {
+    throw new Error("No se han encontrado coincidencias");
+  }
 
-  return coincidencias ? coincidencias : [];
+  return coincidencias;
 };
 
 const extraerURL = (coincidencias: RegExpMatchArray) => {
   const urls = coincidencias.map((coincidencia) => {
-    const urlExec = /src=["']([^"']+)["']/.exec(coincidencia);
-    return urlExec;
+    const urlExec = /src=["']([^"']+)["']/.exec(
+      coincidencia
+    ) as RegExpMatchArray;
+    return urlExec[1];
   });
 
   return urls;
 };
+
+const pintarUrls = (urls: string[]) => {
+  const divresultado = document.querySelector("#resultados");
+  urls.forEach((url) => {
+    const link = document.createElement("a");
+    link.innerHTML = url;
+    link.href = url;
+    divresultado?.appendChild(link);
+  });
+};
+
+const extraerImagenes = (html: string) => {
+  try {
+    const tagsImg = buscarTagsImg(html);
+    const urls = extraerURL(tagsImg);
+    pintarUrls(urls);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const botonExtraerImagenes = document.querySelector(
+    "button"
+  ) as HTMLButtonElement;
+  botonExtraerImagenes.addEventListener("click", () => {
+    const textArea = document.querySelector("textarea") as HTMLTextAreaElement;
+    extraerImagenes(textArea.value);
+  });
+});
